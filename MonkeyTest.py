4b4354451c70a6d365ab07958c5471c76ee654d8
Chandrasekar-JS <chandrasekar.j4@gmail.com>
2016-09-11 13:10:13 +0530

    Added MonkeyTest.py file
    Added GUI Support

--- MonkeyTest.py
+++ MonkeyTest.py
@@ -1,21 +1,40 @@
 from __future__ import print_function
 from subprocess import Popen, PIPE
+import wx
 
 
-class MonkeyTest(object):
-
+class MonkeyTest(wx.Frame):
     """ This class is to build and run the monkey command """
 
     # class constants with static part of monkey command
     cmd = 'adb shell monkey -p '
     verbose_level = ' -v -v -v '
 
-    def __init__(self, package_name, number_of_events):
-        self.package_name = package_name
-        self.events_number = str(number_of_events)
+    def __init__(self, parent, id):
+        self.package_name = None
+        self.events_number = None
         self.monkey_command = None
         self.monkey_events = None
         self.event_file = None
+
+        # wx panel design
+        wx.Frame.__init__(self, parent, id, 'Android Monkey Test', size=(400, 400))
+        monkey_test_panel = wx.Panel(self)
+
+        # UX for User inputs
+        self.basic_label = wx.StaticText(monkey_test_panel, -1, "Package Name:")
+        self.basic_text = wx.TextCtrl(monkey_test_panel, -1, "Enter your app package name", size=(175, -1))
+        self.basic_text.SetInsertionPoint(0)
+        self.num_of_events_label = wx.StaticText(monkey_test_panel, -1, "Number of events:")
+        self.num_of_events = wx.TextCtrl(monkey_test_panel, -1, "200", size=(175, -1))
+        sizer = wx.FlexGridSizer(cols=2, hgap=6, vgap=6)
+        sizer.AddMany([self.basic_label, self.basic_text, self.num_of_events_label, self.num_of_events])
+        monkey_test_panel.SetSizer(sizer)
+
+        # button action
+        run_monkey_button = wx.Button(monkey_test_panel, label='Run', pos=(300, 300), size=(40, 40))
+        self.Bind(wx.EVT_BUTTON, self.runtest, run_monkey_button)
+        self.Bind(wx.EVT_CLOSE, self.closewindow)
 
     def build_monkey_command(self):
         """returns the adb command """
@@ -25,6 +44,7 @@ class MonkeyTest(object):
     def run_monkey_test(self):
         """ adb command, runs the monkey command and saves the monkey events to text file"""
         self.build_monkey_command()
+        print(self.monkey_command)
         process = Popen(self.monkey_command, stdout=PIPE, stderr=PIPE, shell=True)
         stdout, stderr = process.communicate()
         self.monkey_events = stdout
@@ -33,7 +53,19 @@ class MonkeyTest(object):
             self.event_file.write(self.monkey_events)
             self.event_file.close()
         else:
-            print ('error:', stderr)
+            print('error:', stderr)
 
-Monkey_test = MonkeyTest('com.yourPackageName.android', 'replaceWithNumber')
-Monkey_test.run_monkey_test()
+    def runtest(self, event):
+        self.package_name = str(self.basic_text.GetValue())
+        self.events_number = str(self.num_of_events.GetValue())
+        self.run_monkey_test()
+
+    def closewindow(self, event):
+        self.Destroy()
+
+
+if __name__ == '__main__':
+    app = wx.PySimpleApp()
+    frame = MonkeyTest(parent=None, id=-1)
+    frame.Show()
+    app.MainLoop()
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# AndroidMonkeyTool
+# AndroidMonkeyTestTool
 This is a python based GUI tool to run android monkey test.
 User neccessiate to provide the application package name and can pick the necessary monkey commands from GUI tool
